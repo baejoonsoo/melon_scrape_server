deea35fc3e924b6f5dfa770073393910691e5f04
baejoonsoo <artns25@dsm.hs.kr>
2022-04-29 23:44:29 +0900

    melon scrape node server

---
+++ index.js
@@ -0,0 +1,18 @@
+import scrape from "./scrape.js";
+import express from "express";
+
+const app = express();
+
+const port = app.listen(8000);
+
+app.get("/", function (req, res) {
+  res.send("<h1>Express server Start</h1>");
+});
+
+app.get("/melon", async function (_, res) {
+  res.send(await scrape());
+});
+
+app.listen(port, function () {
+  console.log("start! express server");
+});
---
+++ scrape.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import cheerio from "cheerio";
+
+const getDocData = async () => {
+  return await axios
+    .get("https://www.melon.com/chart/")
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => console.log(err));
+};
+
+const getData = async (child) => {
+  const lists = [];
+  const $ = cheerio.load(await getDocData());
+
+  child.each((i, elem) => {
+    const songInfo = $(elem).find(".wrap_song_info");
+
+    const rank = $(elem).find(".t_center > .rank").text().trim();
+    const title = $(songInfo).find(".rank01 > span > a").text().trim();
+    const singer = $(songInfo).find(".rank02 > a").text().trim();
+    const album = $(songInfo).find(".rank03 > a").text().trim();
+
+    lists.push({
+      rank: rank,
+      title: title,
+      singer: singer,
+      album: album,
+    });
+  });
+
+  return lists;
+};
+
+const scrape = async () => {
+  const $ = cheerio.load(await getDocData());
+
+  const table = $("table tbody");
+  const lst50 = await getData(table.find(".lst50"));
+  const lst100 = await getData(table.find(".lst100"));
+
+  const lists = [...lst50, ...lst100];
+  console.log(lists);
+
+  return lists;
+};
+
+export default scrape;
