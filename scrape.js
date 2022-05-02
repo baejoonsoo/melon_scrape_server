000d42747b7c4b480d0e3c15904979785630bcb2
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-05-02 15:17:01 +0900

    cheerio.load() 재사용

--- scrape.js
+++ scrape.js
@@ -10,17 +10,23 @@ const getDocData = async () => {
     .catch((err) => console.log(err));
 };
 
-const getData = async (child) => {
-  const lists = [];
-  const $ = cheerio.load(await getDocData());
+// const getYoutubeUrl = (searchKeyword) => {
+//   const
+// };
 
-  child.each((i, elem) => {
+const getData = async (child, $) => {
+  const lists = [];
+  // const $ = cheerio.load(await getDocData());
+
+  child.each(async (i, elem) => {
     const songInfo = $(elem).find(".wrap_song_info");
 
     const rank = $(elem).find(".t_center > .rank").text().trim();
     const title = $(songInfo).find(".rank01 > span > a").text().trim();
     const singer = $(songInfo).find(".rank02 > span").text().trim();
     const album = $(songInfo).find(".rank03 > a").text().trim();
+
+    // const url = await getYoutubeUrl(`${singer} ${title}`);
 
     lists.push({
       rank: rank,
@@ -37,8 +43,8 @@ const scrape = async () => {
   const $ = cheerio.load(await getDocData());
 
   const table = $("table tbody");
-  const lst50 = await getData(table.find(".lst50"));
-  const lst100 = await getData(table.find(".lst100"));
+  const lst50 = await getData(table.find(".lst50"), $);
+  const lst100 = await getData(table.find(".lst100"), $);
 
   const lists = [...lst50, ...lst100];
   console.log(lists);
